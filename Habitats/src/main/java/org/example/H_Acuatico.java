7db71ec12d7a6a29174f7c9f2e9d78ac1412c5a9
KravAD <dkrav@myuax.com>
2024-03-06 11:19:11 +0100

    commit 4
    Añado constructor que inicializa la clase Habitats.

--- Habitats/src/main/java/org/example/H_Acuatico.java
+++ Habitats/src/main/java/org/example/H_Acuatico.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class H_Acuatico extends Habitats{
 
-
+    
 }
 
--- Habitats/src/main/java/org/example/Habitats.java
+++ Habitats/src/main/java/org/example/Habitats.java
@@ -5,6 +5,12 @@ public class Habitats {
     private boolean limpieza;
     private float humedad;
 
+    public Habitats (){
+        double temperatura = 0.0;
+        boolean limpieza = true;
+        float humedad = 0.1F;
+
+    }
     public Habitats(double temperatura, boolean limpieza, float humedad) {
         this.temperatura = temperatura;
         this.limpieza = limpieza;
