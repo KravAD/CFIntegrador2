d4425d733d3ec6e7bbf0975c8205dc2c880e6124
KravAD <dkrav@myuax.com>
2024-03-10 21:49:15 +0100

    commit 13
    constructor animales

--- Animales/src/main/java/org/example/Animales.java
+++ Animales/src/main/java/org/example/Animales.java
@@ -5,4 +5,9 @@ public class Animales {
     private int edad;
     private String habitat;
 
+    public Animales(String nombre, int edad, String habitat) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.habitat = habitat;
+    }
 }
